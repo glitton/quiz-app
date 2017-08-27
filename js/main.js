a9798ae2cd43bf9d3927b1df83c16513cc5419c5
Generosa Litton <generosa.litton@yahoo.com>
2017-08-26 17:22:31 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+// JavaScript for GS Warriors quiz app
+$(document).ready(function(){
+// question and answer array of objects
+var questions = [
+  {
+    question : "Who are the Warriors' top three point scorers of all time?",
+    answers  : ["Steph Curry, Monta Ellis",  "Steph Curry, Klay Thompson",          "Chris Mullin, Mitch Richmond", "Steph Curry, Kevin Durant"],
+    correct  : "Steph Curry, Klay Thompson"
+  },
+  {
+    question : "What is the paragraph tag used for?",
+    answers  : [" to create paragraph text on a CSS page", " to create heading text on a HTML page", " to create heading text on a CSS page", " to create paragraph text on a HTML page"],
+    correct  : " to create paragraph text on a HTML page"
+  },
+  {
+    question : "What are 3 ways to write CSS?",
+    answers  :  [" External Style Sheet, External Style Tag & Inline Style", " External Style Sheet, Internal Style Tag & Inline Style", " Inline Style Sheet, Internal Style Tag & Inline Style", " External Style Sheet, Internal Style Tag & Inline Style Sheet"],
+    correct  : " External Style Sheet, Internal Style Tag & Inline Style"
+  },
+  {
+    question : "How to declare an HTML file type?",
+    answers  : [" <!CSS doctype>", " <!HTML doctype>", " <!DOCTYPE CSS>", " <!DOCTYPE html>"],
+    correct  : " <!DOCTYPE html>"
+  },
+  {
+    question : "Where in your HTML file do you link to your CSS file?",
+    answers  : [" What is CSS?", " In between the opening and closing head tags", " In between the opening and closing body tags", " In between the opening and closing heading tags"],
+    correct  : " In between the opening and closing head tags"
+  },
+  {
+    question : "How many different heading tags are there?",
+    answers  : [" 6", " 5", " 7", " 2"],
+    correct  :  " 6"
+  },
+  {
+    question : "What does CSS stand for?",
+    answers  : [" Cascading Smile Sheets", " Cascading Style Stuff", " Cascading Style Sheets", " Hypertext Markup Language"],
+    correct  : " Cascading Style Sheets"
+  }
+];
+
+
+
+  // Create 
+  // from https://www.jetpunk.com/user-quizzes/23532/golden-state-warriors-trivia
+  // Create random number generator to generate 5 questions
+  // So it will be different every time
+  // Capture user input radio button
+  // Count how many are correct
+  // Count how many are wrong
+  // Show result when user clicks button
+  // Show answers
+
+});
